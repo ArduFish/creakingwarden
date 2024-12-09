f62ba3870ef88a8fe718cadf1068b056d16aa635
IntelliFish123 <vlad.robert.juganaru@gmail.com>
2024-12-09 18:44:18 +0200

    fish

--- src/main/java/org/ardufish/creakingwarden/mixin/creak.java
+++ src/main/java/org/ardufish/creakingwarden/mixin/creak.java
@@ -1,4 +1,23 @@
 package org.ardufish.creakingwarden.mixin;
 
+import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.mob.CreakingEntity;
+import net.minecraft.entity.mob.HostileEntity;
+import org.spongepowered.asm.mixin.*;
+
+@Mixin(CreakingEntity.class)
 public class creak {
+    @Shadow
+    @Final
+    @Mutable
+    private static final float field_54575 = 40.0F;
+    /**
+     * @author ArduFish
+     * @reason Overwrite damage
+     */
+    @Overwrite
+    public static DefaultAttributeContainer.Builder createCreakingAttributes() {
+        return HostileEntity.createHostileAttributes().add(EntityAttributes.MAX_HEALTH, (double)1.0F).add(EntityAttributes.MOVEMENT_SPEED, (double)0.4F).add(EntityAttributes.ATTACK_DAMAGE, (double)40.0F).add(EntityAttributes.FOLLOW_RANGE, (double)32.0F).add(EntityAttributes.STEP_HEIGHT, (double)1.0625F);
+    }
 }
