3732714c732b72ed8fc362c024de61ae3baa7008
ArduFish123 <80416599+ArduFish@users.noreply.github.com>
2024-12-10 09:59:14 +0200

    Update Creakingwarden.java

--- src/main/java/org/ardufish/creakingwarden/Creakingwarden.java
+++ src/main/java/org/ardufish/creakingwarden/Creakingwarden.java
@@ -4,7 +4,19 @@ import net.fabricmc.api.ModInitializer;
 
 public class Creakingwarden implements ModInitializer {
 
-    @Override
-    public void onInitialize() {
+    public static final String MOD_ID = "creakingwarden";
+
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger(creakingwarden);
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Creakings are now Wardens.");
     }
 }
